2459a83385faa467f94dbc4957caf4a92a6c0393
Kevin Hoogendam <khoogendam6@gmail.com>
2018-08-20 19:06:15 +0200

    70% lines & statements

--- src/app/components/competition/create/create.component.spec.ts
+++ src/app/components/competition/create/create.component.spec.ts
@@ -3,7 +3,9 @@ import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
 import { UUID } from 'angular2-uuid';
 import { CompetitionCreateComponent } from './create.component';
 import { User } from '../../../models/user.model';
-import { TestMocksModule, testUser } from '../../../modules/test/TestMocks.module';
+import { TestMocksModule, testUser, testPouleCompetition } from '../../../modules/test/TestMocks.module';
+import { Competition } from '../../../models/competition.model';
+import { CompetitionService } from '../../../services/competition/competition.service';
 
 
 describe('CreateComponent', () => {
@@ -28,22 +30,26 @@ describe('CreateComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  // it('should create a competition', () => {
-  //   const competitionForm = <NgForm>{
-  //     value: {
-  //       name: 'name',
-  //       startDate: 'Hello',
-  //       type: 'poule',
-  //       ownerId: 'id',
-  //       maxParticipants: '10',
-  //       matchTime: 'World',
-  //       rounds: '12'
-  //     }
-  //   };
+  it('should create a competition', () => {
+    const competitionForm = <NgForm>{
+      value: {
+        name: 'name',
+        startDate: 'Hello',
+        type: 'poule',
+        ownerId: 'id',
+        maxParticipants: '10',
+        matchTime: 'World',
+        rounds: '12'
+      }
+    };
+    component.user = testUser;
+    expect(() => component.createCompetition(competitionForm)).not.toThrow();
+  });
 
-  //   component.user = testUser;
-  //   component.createCompetition(competitionForm);
-  //   expect(component.competition.uid).toBe(testUser.uid);
-  // });
+  it('should return a data object', () => {
+    component.competition = testPouleCompetition;
+    component.user = testUser;
+    expect(component.generateCreateData()).not.toBeNull();
+  });
 });
 
